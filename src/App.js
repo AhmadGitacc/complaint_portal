ba556cf9dfb3caee9a01d9d5ee5213009f87c432
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-23 05:15:22 +0100

    first c

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, FormLabel, Heading, HStack, Image, Input, Text, Textarea, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import emailjs from "emailjs-com";
+
 
 function App() {
+  const [complaintInfo, setComplaintInfo] = useState({
+    title: "",
+    lecturer: "",
+    nature_of_complaint: "",
+    submittedAt: ""
+  })
+
+  const handleChange = (e) => {
+    setComplaintInfo({ ...complaintInfo, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = () => {
+    emailjs.send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      {
+        title: complaintInfo.title,
+        lecturer: complaintInfo.lecturer,
+        nature_of_complaint: complaintInfo.nature_of_complaint,
+        submittedAt: complaintInfo.submittedAt
+      },
+      PUBLIC_KEY
+    );
+
+    alert("Your complaint has been emailed to the management.");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box w='100%' h='100vh' display='flex' justifyContent='center' alignItems='center'>
+
+      <Box as="form" boxShadow='2xl' border='1px' borderRadius='lg' p={4}>
+        <Image src={"/Lincoln.png"} alt='lcn logo' w='445px' h='100px' />
+        <Heading size='xl' noOfLines={1}>Student Complaints Portal</Heading>
+        <Text variant=''>Please complete ther form below for your complaints.</Text>
+        <VStack spacing={4} p={2}>
+          <VStack w='full' spacing={0} align='flex-start'>
+            <FormLabel>Title</FormLabel>
+            <Input variant="filled" onChange={handleChange} required name="title" value={complaintInfo.title} placeholder='Course Registration Problems..' />
+          </VStack>
+
+          <VStack w='full' spacing={0} align='flex-start'>
+            <HStack spacing={0} align='baseline'>
+              <FormLabel>Lecturer's Name</FormLabel>
+              <Text color='GrayText'>(Optional)</Text>
+            </HStack>
+            <Input variant="filled" onChange={handleChange} name="lecturer" value={complaintInfo.lecturer} placeholder='Dr. ....' />
+          </VStack>
+
+          <VStack w='full' spacing={0} align='flex-start'>
+            <FormLabel>Nature of Complaint</FormLabel>
+            <Textarea variant='filled' onChange={handleChange} required name="nature_of_complaint" value={complaintInfo.nature_of_complaint} placeholder='I have an issue ........' />
+          </VStack>
+        </VStack>
+        <Button variant='solid' w='full' onClick={handleSubmit} colorScheme="red">
+          Submit Complaint
+        </Button>
+      </Box>
+
+    </Box>
   );
 }
 
